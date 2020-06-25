57640382d6102d7255e1162cc87146da8e6ad638
klguenth <klguenth@gmail.com>
2020-06-24 22:36:23 -0500

    update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import EditShow from './Components/EditShow/EditShow.js';
 import AddComedian from './Components/AddComedian/AddComedian.js';
 import EditComedian from './Components/EditComedian/EditComedian.js';
 import ComedianList from './Components/ComedianList/ComedianList.js';
+import ComedianDetail from './Components/ComedianDetail/ComedianDetail.js';
 import ApiContext from './ApiContext';
 import { findComedian, findShow } from './helpers.js';
 import config from './config.js';
@@ -21,7 +22,11 @@ export default class App extends React.Component {
     this.state = {
       loggedIn: true,
       shows: [],
-      comedians: []
+      comedians: [],
+      handleDeleteShow: this.handleDeleteShow.bind(this),
+      handleEditShow: this.handleEditShow.bind(this),
+      handleDeleteComedian: this.handleDeleteComedian.bind(this),
+      handleEditComedian: this.handleEditComedian.bind(this)
     };
   }
 
@@ -47,6 +52,26 @@ export default class App extends React.Component {
           error: err.message
         });
       });
+    fetch(`${config.REACT_APP_API_ENDPOINT}/api/comedian/`)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong.');
+        }
+        return res;
+
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          comedians: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+    });
   }
 
   renderRoutes() {
@@ -55,10 +80,11 @@ export default class App extends React.Component {
         <Route exact path='/' component={LandingPage} />
         <Route path='/showList' component={ShowList} shows={this.state.shows} />
         <Route path='/addShow' component={AddShow} />
-        <Route path='/editShow' render={(props) => <EditShow {...props} />} />
+        <Route path='/editShow/:id' render={(props) => <EditShow {...props} />} />
         <Route path='/addComedian' component={AddComedian} />
-        <Route path='/editComedian' component={EditComedian} />
-        <Route path='/comedianList' component={ComedianList} />
+        <Route path='/editComedian/:id' render={(props) => <EditComedian {...props} />} />
+        <Route path='/comedianList' component={ComedianList} comedians={this.state.comedians} />
+        <Route path='/comedianDetail/:id' render={(props) => <ComedianDetail {...props} />} />
       </>
     );
   }
@@ -66,8 +92,8 @@ export default class App extends React.Component {
   //sets state for comedian edits
   handleEditComedian = (comedian) => {
     const index = findComedian(this.state.comedians, comedian.id);
-    const comedians = this.state.comedians;
-    this.setState = ({
+    const comedians = [...this.state.comedians];
+    this.setState ({
       comedians: comedians.splice(index, 1, comedian)
     })
   }
@@ -75,8 +101,8 @@ export default class App extends React.Component {
 //sets state for comedian delete
   handleDeleteComedian = (comedian) => {
     const index = findComedian(this.state.comedians, comedian);
-    const comedians = this.state.comedians;
-    this.setState = ({
+    const comedians = [...this.state.comedians];
+    this.setState ({
       comedians: comedians.splice(index, 1)
     })
   }
@@ -84,17 +110,26 @@ export default class App extends React.Component {
 //sets state for show edits
   handleEditShow = (show) => {
     const index = findShow(this.state.shows, show.id);
-    const shows = this.state.shows;
-    this.setState = ({
-      shows: shows.splice(index, 1, show)
+    // console.log('index', index);
+    // const shows = [...this.state.shows];
+    // console.log('shows before setState', shows);
+    const shows = [...this.state.shows];
+    const updatedShows = shows.splice(index, 1, show);
+    this.setState ({
+      shows: updatedShows
     })
+    console.log('after setSTate', shows)
+    // this.setState ({
+    //   shows: shows.splice(index, 1, show)
+    // })
+    // console.log('shows after setState', shows);
   }
   
 //sets state for show delete
   handleDeleteShow = (show) => {
     const index = findShow(this.state.shows, show);
     const shows = this.state.shows;
-    this.setState = ({
+    this.setState ({
       shows: shows.splice(index, 1)
     })
   }
--- src/Components/EditShow/EditShow.js
+++ src/Components/EditShow/EditShow.js
@@ -4,15 +4,16 @@ import ApiContext from '../../ApiContext.js';
 import './EditShow.css';
 
 export default class EditShow extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            shows: this.props.shows
-        }
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         shows: this.props.shows
+    //     }
+    // }
 
     static defaultProps = {
         editShow: () => {},
+        deleteShow: () => {},
         match: {
             params: {}
         },
@@ -23,7 +24,7 @@ export default class EditShow extends React.Component {
     handleEditShow = event => {
         event.preventDefault()
         const id = this.props.match.params.id;
-        const index = this.findById(id);
+        const index = this.findById(parseInt(id));
         const showId = this.context.shows[index].id;
         const modifiedShow = {};
         modifiedShow.title = event.target.title.value;
@@ -37,7 +38,7 @@ export default class EditShow extends React.Component {
         modifiedShow.price_general = event.target.price_general.value;
         modifiedShow.capacity = event.target.capacity.value;
         modifiedShow.comps = event.target.comps.value;
-        modifiedShow.tix_id = event.target.tix_id.value;
+        // modifiedShow.tix_id = event.target.tix_id.value;
         fetch(`${config.REACT_APP_API_ENDPOINT}/api/show/${showId}`, {
             method: 'PATCH',
             headers: {
@@ -55,26 +56,61 @@ export default class EditShow extends React.Component {
             modifiedShow.id = id;
             this.context.editShow(modifiedShow)
             this.props.history.push(`/showList`);
+        })
+        .catch(error => {
+            console.error({ error })
         });
     }
 
+    //delete show from show list
+    handleDeleteShow = e => {
+        e.preventDefault()
+        const showId = this.props.match.params.id;
+        fetch(`${config.REACT_APP_API_ENDPOINT}/api/show/${showId}`, {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json'
+            },
+        })
+        .then( res => {
+            if (!res.ok)
+                return res.json().then(e => Promise.reject(e))
+            return res;
+        })
+        .then(() => {
+            this.context.deleteShow(showId);
+            this.props.history.push('/showList')
+        })
+        .catch(error => {
+            console.error({ error })
+        })
+    }
+
     findById(id) {
-        console.log(typeof id, id, 'type of id');
-        console.log(this.context.shows, 'this.context.shows');
         for (let i = 0; i<this.context.shows.length; i++) {
             if (parseInt(id) === this.context.shows[i].id) {
                 return i;
             }
-            console.log(id, 'findbyid id');
-            console.log(parseInt(id), 'parseint')
         }
     }
 
     render() {
-        let id = this.props.match.params.id;
-        let index = this.findById(id)
-        console.log(index, 'index');
+        const show = this.context.shows.find(
+            // converts both sides to integers
+            show => +show.id === +this.props.match.params.id
+        );
+    
+        //  Technically the better way would be to have a marker called isFetching 
+        // and display the appropriate content based on it because if there were legitimately no shows, the user would just have a blank screen.
+        if (this.context.shows.length === 0) {
+            return null;
+        }
+    
         return (
+        // let id = this.props.match.params.id;
+        // let index = this.findById(id)
+        // const show = this.context.shows.find((show) => show.id === this.props.match.params.id)
+        // return (
             <ApiContext.Consumer>
                 {defaultValue => (
                     <div>
@@ -82,56 +118,60 @@ export default class EditShow extends React.Component {
                             <h1>Edit Show</h1>
                         </header>
                         <section className="record-show">
-                            <form onSubmit={this.handleEditShow} action=''>
+                            <form onSubmit={this.handleEditShow}>
                                 <div className="form-section">
                                     <label htmlFor="title">Title</label>
-                                    <input type="text" id="title" name="title" defaultValue={this.context.shows[index].title} required />
+                                    <input type="text" id="title" name="title" defaultValue={show.title} required />
                                 </div>
                                 <div className="form-section">
                                     <label htmlFor="show_date">Date</label>
-                                    <input type="date" id="date" name="show_date" min="2020-04-25" max="2050-01-01" defaultValue={this.context.shows[index].show_date} required/>
+                                    <input type="date" id="show_date" name="show_date" min="2020-04-25" max="2050-01-01"
+                                        defaultValue={show.show_date.slice(0, 10)} required/>
                                 </div>
                                 <div className="form-section">
                                     <label htmlFor="show_time">Time</label>
-                                    <input type="time" id="show_time" name="show_time" min="12:00" max="23:00" defaultValue={this.context.shows[index].show_time} required/>
+                                    <input type="time" id="show_time" name="show_time" min="12:00" max="23:00" defaultValue={show.show_time} required/>
                                 </div>
                                 <div className="form-section">
                                     <label htmlFor="comics">Comics</label>
-                                    <input type="number" defaultValue={this.context.shows[index].comics} name="comics" id="comics"/>
+                                    <input type="number" defaultValue={show.comics} name="comics" id="comics"/>
                                 </div>
                                 <div className="form-section">
                                 <label htmlFor="stage">Stage</label>
-                                    <select name="stage" id="stage" defaultValue={this.context.shows[index].stage}>
+                                    <select name="stage" id="stage" defaultValue={show.stage}>
                                         <option value="patio">patio</option>
                                         <option value="showroom">showroom</option>
                                     </select>
                                 </div>
                                 <div className="form-section">
                                     <label htmlFor="details">Details</label>
-                                    <input type="text" id="details" name="details" defaultValue={this.context.shows[index].details} required />
+                                    <input type="text" id="details" name="details" defaultValue={show.details} required />
                                 </div>
                                 <div className="form-section">
                                     <label htmlFor="notes">Notes</label>
-                                    <textarea name="notes" rows="10" id="notes" defaultValue={this.context.shows[index].notes} required></textarea>
+                                    <textarea name="notes" rows="10" id="notes" defaultValue={show.notes} required></textarea>
                                 </div>
                                 <div className="form-section">
                                     <label htmlFor="price_general">General Price</label>
-                                    <input type="number" min="1" step="any" name="price_general" id="price_general" defaultValue={this.context.shows[index].price_general} />
+                                    <input type="number" min="1" step="any" name="price_general" id="price_general"
+                                        defaultValue={Number(show.price_general.slice(1))} />
                                 </div>
                                 <div className="form-section">
                                     <label htmlFor="price_premium">Premium Price</label>
-                                    <input type="number" min="1" step="any" name="price_premium" id="price_premium" defaultValue={this.context.shows[index].price_premium} />
+                                    <input type="number" min="1" step="any" name="price_premium" id="price_premium"
+                                        defaultValue={Number(show.price_premium.slice(1))} />
                                 </div>
                                 <div className="form-section">
                                     <label htmlFor="capacity">Capacity</label>
-                                    <input type="number" min="1" step="any" name="capacity" id="capacity" defaultValue={this.context.shows[index].capacity} />
+                                    <input type="number" min="1" step="any" name="capacity" id="capacity" defaultValue={show.capacity} />
                                 </div>
                                 <div className="form-section">
                                     <label htmlFor="comps">Comps</label>
-                                    <input type="number" min="1" step="any" name="comps" id="comps" defaultValue={this.context.shows[index].comps} />
+                                    <input type="number" min="1" step="any" name="comps" id="comps" defaultValue={show.comps} />
                                 </div>
                                 <button type="reset">Reset</button>
                                 <button type="submit">Submit</button>
+                                <button type="delete" onClick={this.handleDeleteShow}>Delete</button>
                             </form>
                         </section>
                     </div>
